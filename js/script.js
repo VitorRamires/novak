e552bd1c851d75e6c6fa1a210bb8f8c2df2b2d8a
VitorRamires <vmr.rs@hotmail.com>
2023-11-13 20:28:34 -0300

    remove firebase

--- js/script.js
+++ js/script.js
@@ -7,20 +7,3 @@ animationHandle()
 changeSlide()
 menuMobileHandle()
 changeAdicionais()
-
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDHCjzQEEZvbPWzy1eLRU9FitI1ZH8jA6c",
-  authDomain: "novak-57d92.firebaseapp.com",
-  projectId: "novak-57d92",
-  storageBucket: "novak-57d92.appspot.com",
-  messagingSenderId: "200999826977",
-  appId: "1:200999826977:web:46dd411ae76c8c13afd560",
-  measurementId: "G-32GGV08HYT"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
